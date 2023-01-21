69dad531be3e919b237207119f5f5906d7bca8e8
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-01-20 23:58:49 -0300

    feat: create session function to do login and logout too

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import "express-async-errors"
 import express         from "express";
 import usersRoutes     from "./routes/user.routes";
+import sessionRoutes from "./routes/session.routes";
 import { handleError } from "./errors/error";
 
 const app = express()
 app.use(express.json())
 
 app.use("/user", usersRoutes)
+app.use("", sessionRoutes)
 
 app.use(handleError)
 
